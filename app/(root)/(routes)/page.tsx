d9bb7911098ceeec87aacd855b02d38285e2d029
Rajan <rajanj448@student.sfit.ac.in>
2024-03-17 17:13:30 +0530

    Clerk Authentication

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,7 +1,8 @@
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
-   <h1>Ai</h1>
+   <UserButton afterSignOutUrl="/" />
   );
 }
